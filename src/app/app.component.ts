faa42971d8c3aeb7b1808d3f24ab00fae51c7c20
kbhyana <kbhyana@gmail.com>
2019-04-05 19:15:02 +0530

    Functionality working need to plug into the dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Data } from './models/data.model';
+import {FormBuilder, FormGroup } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
+// import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
+
 export class AppComponent {
-
-  constructor (private httpService: HttpClient) { }
-
+  selectedLevel = '';
+  data:Array<Object> = [
+      {id: 0, name: "Age"},
+      {id: 1, name: "State"},
+      {id: 2, name: "Tenure"}
+  ];
+  data1: Observable<Data>;
+  countryForm: FormGroup;
+  countries = ['State', 'Age', 'Tenure']
+  // constructor() {}
+  constructor (private httpService: HttpClient, private fb: FormBuilder) { 
+    // this.data1 = this.httpService.get<Data>('./assets/data.json');
+  }
   labels =  [];
-
   // OBJECT FOR datasets WITH EMPTY data.
   chartData = [
     {
@@ -27,34 +43,50 @@ export class AppComponent {
    // CHART COLOR.
    colors = [
     { // 1st Year.
-      backgroundColor: 'rgba(77,83,96,0.2)'
+      backgroundColor: 'rgba(30, 16, 224, 0.8)'
     },
     { // 2nd Year.
       backgroundColor: 'rgba(30, 169, 224, 0.8)'
     }
    ]
-  
-    ngOnInit () {
-        this.httpService.get('./assets/count.json', {responseType: 'json'}).subscribe(
+   selected(){
+    // console.log(this.selectedLevel);
+      this.onLoad(this.selectedLevel["name"]);
+    
+  }
+ 
+    onLoad(field) {
+      this.httpService.get('./assets/count.json', {responseType: 'json'}).subscribe(
         data => {
           let i;
           let label = [];
           let datastate = [];
-          for (const key of Object.keys(data['customersAtRiskByState']['customersAtHighRiskByState'])) {
-            label.push(data['customersAtRiskByState']['customersAtHighRiskByState'][key].name)
-            datastate.push(data['customersAtRiskByState']['customersAtHighRiskByState'][key].count)
+          for (const key of Object.keys(data["customersAtRiskBy"+field]["customersAtHighRiskBy"+field])) {
+            label.push(data["customersAtRiskBy"+field]["customersAtHighRiskBy"+field][key].name)
+            datastate.push(data["customersAtRiskBy"+field]["customersAtHighRiskBy"+field][key].count)
         } 
           this.labels = label as any [] ;
           console.log(label) ;
           let datachart = [{"label":"Count","data": datastate}]
           this.chartData = datachart as any [] ;	 // FILL THE CHART ARRAY WITH DATA.
           console.log(datachart)
+          // this.countryForm = this.fb.group({
+          //   countryControl: ['Canada']
+          // });
           },
         (err: HttpErrorResponse) => {
             console.log (err.message);
         }
         );
+      }
+    ngOnInit () {
+        this.onLoad("State"); 
     }
+    
+
+
+
+
 
   onChartClick(event) {
     console.log(event);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ChartsModule } from 'ng2-charts'
+import { ChartsModule } from 'ng2-charts';
+import { BarChartComponent } from './bar-chart/bar-chart.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
+    // FormGroup,
+    // FormBuilder 
   ],
   imports: [
     BrowserModule,
     ChartsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
